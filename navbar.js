de8186bcaf27e16173a96fe52f39901016a682a6
Anirudh Nagraj <nagarajan.anirudh@gmail.com>
2020-10-10 20:01:17 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,4 +1,31 @@
 <script>
+
+  $(function(){
+    if($(window).width() < 768){
+      $(".navbar-nav li span.trigger").on("click",function(e){
+        console.log('triggered');
+            var current=$(this).next();
+        var currentParent=$(this).parent();
+            var grandparent=$(this).parent().parent();
+        console.log(current.attr('class'));
+        
+            if($(this).hasClass('glyphicon-plus')||$(this).hasClass('glyphicon-minus'))
+                $(this).toggleClass('glyphicon-plus glyphicon-minus');
+          
+        
+            grandparent.find('.glyphicon-minus').not(this).toggleClass('glyphicon-plus glyphicon-minus');
+            grandparent.find(".open").not(currentParent).removeClass('open');
+        
+        if(currentParent.hasClass('open')){
+          currentParent.removeClass('open');
+        } else {
+          currentParent.addClass('open');
+        } 
+        });
+        
+    }
+  });
+  
 $( document ).ready(function() {
 
     var opcionesnav = $('.navoption').length;
