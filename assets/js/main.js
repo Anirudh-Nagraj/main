b3a2f716eb1ad4c7762d3ba713cd3f36dd42fccd
Anirudh Nagraj <nagarajan.anirudh@gmail.com>
2023-01-09 19:09:26 -0600

    Major changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,5 +189,35 @@
     });
 });
 
+$(document).ready(function() {
+    checkDisplay();
 
+  $(window).on('resize scroll', function() {
+    checkDisplay();
+  });
+});
+
+function checkDisplay(){
+  $('.prg-count').each(function() {
+      var $this = $(this);
+      if ($this.isOnScreen()) {
+        var countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        }, {
+          duration: 4000,
+          easing: 'linear',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+        });
+      }
+    });
+}
 })(jQuery);
