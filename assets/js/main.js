53784d650b2f248ec49754f51e813b3eee787b9c
Anirudh Nagraj <nagarajan.anirudh@gmail.com>
2023-01-09 19:30:43 -0600

    Changes to Rapid Research page

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,35 +189,18 @@
     });
 });
 
-$(document).ready(function() {
-    checkDisplay();
-
-  $(window).on('resize scroll', function() {
-    checkDisplay();
-  });
-});
-
-function checkDisplay(){
-  $('.prg-count').each(function() {
-      var $this = $(this);
-      if ($this.isOnScreen()) {
-        var countTo = $this.attr('data-count');
-        $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
+$('.counter-count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
         }, {
-          duration: 4000,
-          easing: 'linear',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
+
+          //chnage count up speed here
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
         });
-      }
     });
-}
+
 })(jQuery);
